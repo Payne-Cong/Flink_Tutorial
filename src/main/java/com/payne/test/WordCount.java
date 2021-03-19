2ed15eef12f60e9b663173de315708fc89cd3f16
Payne-Cong <lee.pl2020@outlook.com>
2021-03-19 21:47:42 +0800

    添加  map 和 filter 算子

--- src/main/java/com/payne/test/WordCount.java
+++ src/main/java/com/payne/test/WordCount.java
@@ -1,10 +1,13 @@
 package com.payne.test;
 
+import org.apache.flink.api.common.functions.FilterFunction;
 import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.api.java.DataSet;
 import org.apache.flink.api.java.ExecutionEnvironment;
-import org.apache.flink.api.java.operators.AggregateOperator;
 import org.apache.flink.api.java.operators.DataSource;
+import org.apache.flink.api.java.operators.FlatMapOperator;
+import org.apache.flink.api.java.operators.MapOperator;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.util.Collector;
 
@@ -25,6 +28,34 @@ public class WordCount {
                 .sum(1);// 按照第二个位置 sum
 
         result.print();
+
+        FlatMapOperator<String, String> colData = dataSource.flatMap(new FlatMapFunction<String, String>() {
+            @Override
+            public void flatMap(String value, Collector<String> out) throws Exception {
+                String[] strings = value.split(" ");
+                for (String string : strings) {
+                    out.collect(string);
+                }
+            }
+        });
+
+        // 先转换成单列,在map转(k,k.length)
+        MapOperator<String, Tuple2<String, Integer>> mapOperator = colData.map(new MapFunction<String, Tuple2<String,Integer>>() {
+            @Override
+            public Tuple2<String, Integer> map(String value) throws Exception {
+                return new Tuple2<>(value,value.length());
+            }
+        });
+
+        mapOperator.print();
+
+        //过滤
+        colData.filter(new FilterFunction<String>() {
+            @Override
+            public boolean filter(String value) throws Exception {
+                return !value.contains("hello");
+            }
+        }).print();
 
 
     }
